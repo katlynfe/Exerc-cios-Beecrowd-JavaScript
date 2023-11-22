a88709ade8ac2fffedd9b8e5a8d784268245d27c
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:16:02 -0300

    Create Exercício1172.js

---
+++ Exercício1172.js
@@ -0,0 +1,27 @@
+/*
+Faça um programa que leia um vetor X[10]. Substitua a seguir, todos os valores nulos e negativos do vetor X por 1. Em seguida mostre o vetor X.
+
+Entrada
+A entrada contém 10 valores inteiros, podendo ser positivos ou negativos.
+
+Saída
+Para cada posição do vetor, escreva "X[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
+*/
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+for (let i = 0; i < lines.length; i++) {
+    if (lines[i] <= 0){
+        lines[i] = 1
+    }
+    console.log(`X[${i}] = ${Number(lines[i])}`)
+}
+
+
