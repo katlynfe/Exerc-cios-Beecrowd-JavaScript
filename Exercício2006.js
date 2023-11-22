3c3c9ce8470d8796fcd2ba4bc1310dc7376bc7d5
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:16:28 -0300

    Create Exercício2006.js

---
+++ Exercício2006.js
@@ -0,0 +1,55 @@
+/*
+Degustação de chá às escuras é a habilidade de identificar um chá usando apenas seus sentidos do olfato e paladar.
+
+Isto faz parte da Competição Ideal de Consumidores de Chá Puro (da sigla em inglês ICPC), que um programa de TV local está organizando. Durante o show, um bule de chá completo é preparado e são entregues uma xícara de chá para cada um dos cinco competidores. Os participantes devem cheirar, saborear e avaliar a amostra, de modo a identificar o tipo de chá, que pode ser: (1) o chá branco; (2) chá verde; (3) chá preto; ou (4) chá de ervas. No final, as respostas são verificadas para determinar o número de suposições corretas.
+
+Dado o tipo de chá real e as respostas fornecidas, determinar o número de participantes que receberam a resposta correta.
+
+Entrada
+A primeira linha contém um inteiro T representando o tipo de chá (1 ≤ T ≤ 4). A segunda linha contém cinco inteiros A, B, C, D e E, que indica a resposta dada por cada competidor (1 ≤ A, B, C, D, E ≤ 4).
+
+Saída
+A saída contém um inteiro representando o número de concorrentes que obtiveram a resposta correta.
+*/
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+let cha = lines[0]
+let line1 = lines[1].split(" ")
+
+let a = line1[0]
+let b = line1[1]
+let c = line1[2]
+let d = line1[3]
+let e = line1[4]
+
+let contador = 0
+
+if (cha == a){
+    contador++
+}
+
+if (cha == b){
+    contador++
+}
+
+if (cha == c){
+    contador++
+}
+
+if (cha == d){
+    contador++
+}
+
+if (cha == e){
+    contador++
+}
+
+console.log(contador)
