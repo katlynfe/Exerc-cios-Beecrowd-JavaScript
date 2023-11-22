78c09945a38be140062ed0cb71d6949677a13996
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:15:53 -0300

    Create Exercício1155.js

---
+++ Exercício1155.js
@@ -0,0 +1,27 @@
+/*
+Escreva um algoritmo para calcular e escrever o valor de S, sendo S dado pela fórmula:
+S = 1 + 1/2 + 1/3 + … + 1/100
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+A saída contém um valor correspondente ao valor de S.
+O valor deve ser impresso com dois dígitos após o ponto decimal.
+*/
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+let divisao = 0
+for (let i = 1; i <= 100; i++) {
+    divisao += 1/i
+    
+}
+console.log(divisao.toFixed(2))
