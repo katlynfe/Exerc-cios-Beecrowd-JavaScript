f819bdb0bbe68301750e66b75ea02ad686025452
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:15:57 -0300

    Create Exercício1157.js

---
+++ Exercício1157.js
@@ -0,0 +1,28 @@
+/*
+Ler um número inteiro N e calcular todos os seus divisores.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Escreva todos os divisores positivos de N, um valor por linha.
+*/
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+let N = lines[0];
+
+for (let i = 1; i <= N; i++){
+    if (N % i == 0) {
+        console.log(i)
+    }  
+}
+
+
