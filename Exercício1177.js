a856845987a9b0d0bcb9b141646980e7ec7b0026
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:16:13 -0300

    Create Exercício1177.js

---
+++ Exercício1177.js
@@ -0,0 +1,29 @@
+/*
+Faça um programa que leia um valor T e preencha um vetor N[1000] com a sequência de valores de 0 até T-1 repetidas vezes, conforme exemplo abaixo. Imprima o vetor N.
+
+Entrada
+A entrada contém um valor inteiro T (2 ≤ T ≤ 50).
+
+Saída
+Para cada posição do vetor, escreva "N[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
+*/
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+let T = Number(lines[0])
+let x = 0
+
+for (let i = 0, x = 0; i < 1000; i++,x++) {
+    if (x >= T){
+        x = 0
+    }
+
+    console.log(`N[${i}] = ${x}`)
+}
