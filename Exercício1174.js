451d88fe37441dc2aac1288427adf2f0317a6152
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:16:07 -0300

    Create Exercício1174.js

---
+++ Exercício1174.js
@@ -0,0 +1,24 @@
+/*
+Faça um programa que leia um vetor A[100]. No final, mostre todas as posições do vetor que armazenam um valor menor ou igual a 10 e o valor armazenado em cada uma das posições.
+
+Entrada
+A entrada contém 100 valores, podendo ser inteiros, reais, positivos ou negativos.
+
+Saída
+Para cada valor do vetor menor ou igual a 10, escreva "A[i] = x", onde i é a posição do vetor e x é o valor armazenado na posição, com uma casa após o ponto decimal.
+*/
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+for (let i = 0; i < (lines.length - 1); i++) {
+    if (lines[i] <= 10){
+        console.log(`A[${i}] = ${Number(lines[i]).toFixed(1)}`)
+    }
+}
