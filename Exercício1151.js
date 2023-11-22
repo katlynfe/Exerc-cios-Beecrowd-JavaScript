fbadcdf639c0d4f16fed38fceab712699ac38c5f
katlynfe <117413725+katlynfe@users.noreply.github.com>
2023-11-22 14:15:49 -0300

    Create Exercício1151.js

---
+++ Exercício1151.js
@@ -0,0 +1,30 @@
+/*
+A seguinte sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como série de Fibonacci. Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores. Escreva um algoritmo que leia um inteiro N (N < 46) e mostre os N primeiros números dessa série.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N (0 < N < 46).
+
+Saída
+Os valores devem ser mostrados na mesma linha, separados por um espaço em branco. Não deve haver espaço após o último valor.
+*/
+
+
+
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+let N = Number(lines[0])
+let FIBONACCI = [0, 1]
+for (let i = 2; i < N; i++) {
+    let prox = FIBONACCI[i-1] + FIBONACCI[i-2]
+    FIBONACCI[i] = prox
+    
+}
+
+let string = FIBONACCI.join(' ');
+console.log(string)
